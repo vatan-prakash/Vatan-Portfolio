a0d4cd5abf95d2532f7848c9292e19a7a5a5f33e
Vatan Prakash <vatanprakash867@gmail.com>
2023-10-27 17:12:28 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+// crating a tabbed component
+const port_btn = document.querySelector(".p-btns");//to target id and class
+const p_btn = document.querySelectorAll(".p-btn");//for all class of p-btn
+const img_div = document.querySelectorAll(".img-ovelay");//for class of overlay
+
+port_btn.addEventListener("click", (e) => {
+    // console.log(e.target);
+    // we will get which child element was clicked
+    const p_btn_clicked = e.target;
+    console.log(p_btn_clicked);
+
+    if (!p_btn_clicked.classList.contains("p-btn")) return;
+    // always remove the classList first then add the class
+    p_btn.forEach((curElem) => curElem.classList.remove("p-btn-active"));
+    // img_div.forEach((curElem) =>
+    //   curElem.classList.remove("portfolio-image-active")
+    // );
+
+    p_btn_clicked.classList.add("p-btn-active");
+
+    // to find the p-img class number of the images using the btn data attribute number
+
+    const btn_num = p_btn_clicked.dataset.btnNum;
+    // console.log(btn_num);
+
+    const img_active = document.querySelectorAll(`.p-btn--${btn_num}`);
+    // console.log(img_active);
+
+    img_div.forEach((curElem) =>
+        curElem.classList.add("portfolio-image-not-active")
+    );
+
+    img_active.forEach((curElem) =>
+        curElem.classList.remove(`portfolio-image-not-active`)
+    );
+});
+
+// ========================================
+//  lazy loading section
+// ========================================
+const imgRef = document.querySelector("img[data-src]");
+console.log(imgRef);
+
+const lazyImg = (entries) => {
+    const [entry] = entries;
+    if (!entry.isIntersecting) return;
+    entry.target.src = imgRef.dataset.src;
+};
